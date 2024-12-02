d48678cd64b79b4eaecda0c7ec86b54c1d7c950a
Lucas Souza <lucascorreia325@gmail.com>
2024-12-02 15:57:33 -0300

    feat: adjusts

--- src/js/script.js
+++ src/js/script.js
@@ -50,22 +50,22 @@ btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
 const themeButton = document.getElementById('theme-button')
-const lightTheme = 'light-theme'
+const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
 
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-sun' : 'bx bx-sun'
 
 if (selectedTheme) {
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 themeButton.addEventListener('click', () => {
-    document.body.classList.toggle(lightTheme)
+    document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
